2bd1d6f04196c60b95cc9b720b3c6579998ac4b0
Will Lied <wdlied@gmail.com>
2020-02-28 17:34:27 -0500

    Social Sharing exercise

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList, Modal, 
-    Button, StyleSheet, Alert, PanResponder } from 'react-native';
+    Button, StyleSheet, Alert, PanResponder,
+    Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -63,6 +64,16 @@ function RenderCampsite(props) {
         }
     })
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title, 
+            message: `${title}: ${message} ${url}`,
+            url
+        }, {
+            dialogTitle: 'Share ' + title
+        });
+    };
+
     if (campsite) {
         return (
             <Animatable.View 
@@ -96,6 +107,16 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            style={styles.cardItem}
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl +
+                                campsite.image)}
                         />
                     </View>
                 </Card>
